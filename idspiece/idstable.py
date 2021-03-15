bd09190a6fe70abb9c3d7c2f4555ea179f6b225c
Koichi Yasuoka <yasuoka@kanji.zinbun.kyoto-u.ac.jp>
2021-03-15 19:34:59 +0900

    python 3.6 support

--- idspiece/idstable.py
+++ idspiece/idstable.py
@@ -2,4 +2,7 @@ from .cjk0 import cjk0
 from .cjk1 import cjk1
 from .cjk2 import cjk2
 from .cjk3 import cjk3
-idstable=cjk0|cjk1|cjk2|cjk3
+idstable=cjk2
+idstable.update(cjk3)
+idstable.update(cjk1)
+idstable.update(cjk0)
