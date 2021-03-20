b9d7d18d6f634d02fcf35d15dd474b4a1c37d3c0
Koichi Yasuoka <yasuoka@kanji.zinbun.kyoto-u.ac.jp>
2021-03-21 00:38:14 +0900

    version 0.5.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ URL="https://github.com/KoichiYasuoka/IDSpiece"
 
 setuptools.setup(
   name="idspiece",
-  version="0.4.4",
+  version="0.5.0",
   description="Ideographic Tokenizer with CHISE-IDS",
   long_description=long_description,
   long_description_content_type="text/markdown",
